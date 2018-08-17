ac05d2a6ba449823cac641df51e1da7b105201b8
sergei <asfitsfz3@yandex.ru>
2018-08-17 05:04:47 +0300

    8

---
+++ t5.php
@@ -0,0 +1,28 @@
+<?php
+$bmw = array(
+    "model" => "X5",
+    "speed" => "120",
+    "doors" => "5",
+    "year"  => "2015"
+);
+$toyota = array(
+    "model" => "Fsdfs",
+    "speed" => "300",
+    "doors" => "24",
+    "year"  => "2115"
+);
+$opel = array(
+    "model" => "oploap",
+    "speed" => "1300",
+    "doors" => "1",
+    "year"  => "1842"
+);
+$cars = array (
+    "bmw"    => $bmw,
+    "toyota" => $toyota,
+    "opel"   => $opel
+);
+foreach ($cars as $key => $value) {
+    echo "CAR ".$key.PHP_EOL;
+    echo $value["model"]." ".$value["speed"]." ".$value["doors"]." ".$value["year"].PHP_EOL.PHP_EOL;
+}
